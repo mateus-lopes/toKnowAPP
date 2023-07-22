15fa5ad60baced1131ea4fb80ce25dc0ada65da0
Mateus Lopes Albano <50590861+mateus-lopes@users.noreply.github.com>
2023-07-22 05:58:58 +0000

    instalação do vue/vue-router

---
+++ frontend/src/views/GameView.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        game view
+        <router-link to="/">
+            <button>VOLTAR</button>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        home view
+        <router-link to="/game">
+            <button>Start Game</button>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
